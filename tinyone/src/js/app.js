916b2d99041be792e3b22d205c70804ff0c5041d
Kelvin <grishakelvin@gmail.com>
2019-12-01 22:15:47 +0200

    tinyone v.3

--- tinyone/src/js/app.js
+++ tinyone/src/js/app.js
@@ -1,5 +1,21 @@
 const words = document.querySelectorAll('.th-main__list-desc');
+console.log(words);
 Array.from(words).forEach((desc) => {
-const substr = desc.outerText.substring(0, 100) + '…';
-card.innerText = substr;
+	const substr = desc.outerText.substring(0, 120) + '…';
+	desc.innerText = substr;
 });
+
+const form = document.querySelector('.form');
+const answer = document.querySelector('.th-footer__title');
+
+function handleSubmit(event) {
+
+	event.preventDefault();
+
+
+	const mail = form.elements.mail;
+
+	answer.innerText = mail;
+}
+
+
