a291754860270020a1d29f869feccbe73ef5562f
Kelvin <grishakelvin@gmail.com>
2020-01-19 23:39:42 +0200

    ducks v.2 final

--- ducks/src/js/app.js
+++ ducks/src/js/app.js
@@ -14,3 +14,47 @@ function closeMenu(event) {
 };
 
 
+const mail = document.querySelector(".dk-sub__text");
+const form = document.querySelector(".dk-sub__form");
+const title = document.querySelector(".dk-sub__title");
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const data = {};
+	const text = form.elements.text;
+
+	for(let i = 0; i < form.elements.length; i++) {
+		if(form.elements[i].name === 'submit') {
+			continue;
+		}
+		data[form.elements[i].name] = form.elements[i].value;
+	}
+
+	if (data.text === "") {
+		title.innerText = `Write something`;
+	} else {title.innerText = `${data.text}`;}
+
+});	
+
+const subtitle = document.querySelector(".dk-sub__subtitle");
+form.addEventListener('change', (event) => {
+	const data = {};
+	const text = form.elements.text;
+
+	for(let i = 0; i < form.elements.length; i++) {
+		if(form.elements[i].name === 'submit') {
+			continue;
+		}
+		data[form.elements[i].name] = form.elements[i].value;
+	}
+
+	subtitle.innerText = `${data.text}`;
+});
+
+// const menuFooter = document.querySelector(".dk-footer__list-link");
+// menuFooter.addEventListener('mouseover', () => {
+// 	menuFooter.classList.toggle("mouseover");
+// });
+
+// menuFooter.addEventListener('mouseout', () => {
+// 	menuFooter.classList.toggle("mouseout");
+// });
