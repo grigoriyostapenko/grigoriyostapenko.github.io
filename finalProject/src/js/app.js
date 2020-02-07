4700588494f287f59b2c932e778c45144f312896
Kelvin <grishakelvin@gmail.com>
2020-02-07 18:30:01 +0200

    final project DONE

--- finalProject/src/js/app.js
+++ finalProject/src/js/app.js
@@ -3,7 +3,8 @@ fetch('https://my-json-server.typicode.com/grigoriyostapenko/fake-database/menu'
   function (response) { 
     response.json().then(function(data) { 
       for (let i = 0; i < data.length; i++) {
-        $(".fp-header__list").append('<li class="fp-header__list-item"><a href="#" class="fp-header__list-link">' + data[i] + '</a></li>');
+        $(".fp-header__list").append('<li class="fp-header__list-item"><a href="#'+data[i]+'" class="fp-header__list-link">' + data[i] + '</a></li>');
+        $("#mySidenav").append('<a class="sidenav__link" href="#'+data[i]+'">'+data[i]+'<a>')
       }
     });  
   }  
@@ -29,4 +30,102 @@ function preloader() {
   preloaderBlock.style.display = 'none';
 };
 
-setTimeout(preloader, 5000);
+setTimeout(preloader, 100);
+
+const closeTimes = document.querySelector(".closebtn");
+const openBars = document.querySelector(".fp-menu__bars");
+
+openBars.addEventListener('click', openNav);
+closeTimes.addEventListener('click', closeNav);
+
+
+function openNav(event) {
+  event.preventDefault();
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.paddingLeft = "20px";
+}
+
+function closeNav(event) {
+  event.preventDefault();
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.paddingLeft = "0px";
+}
+
+function closeMenu() {
+  const menuClose = document.querySelector(".sidenav__link");
+  menuClose.addEventListener('click', closeNav);
+}
+
+setTimeout(closeMenu, 5000);
+
+
+let lastScrollTop = 0;
+
+$(window).scroll(function(event){
+
+  let st = $(this).scrollTop();
+  
+  if (st < 100){
+    $("#btnTop").removeClass('active');
+  } else {
+    if (st > lastScrollTop){
+      $("#btnTop").removeClass('active');
+    } else {
+      $("#btnTop").addClass('active');
+    }   
+  }
+  
+  lastScrollTop = st;
+  
+});
+
+
+// // Scroll to bottom on document ready
+// $(document).ready(function(){
+//   $("html, body").animate({ scrollTop: $(document).height() }, 200);
+//   return false;
+// });
+
+
+// // Smooth scroll
+// $(document).ready(function(){
+//   $('a[href^="#"]').on('click',function (e) {
+//     e.preventDefault();
+//     const target = this.hash,
+//     $target = $(target);
+//     $('html, body').stop().animate({
+//       'scrollTop': $target.offset().top
+//     }, 200, function () {
+//       window.location.hash = target;
+//     });
+//   });
+// });
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+
+
+
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
+
+today = dd + '-' + mm + '-' + yyyy;
+console.log(today);
+
+const dateControl = document.getElementById("start")
+dateControl.value = today;
