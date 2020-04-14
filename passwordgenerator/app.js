a6c418a8cbddcff018b6b1c182fbfe126f119df5
Kelvin <grishakelvin@gmail.com>
2020-04-14 20:51:09 +0300

    password generator DONE

---
+++ passwordgenerator/app.js
@@ -0,0 +1,33 @@
+const generateButton = document.querySelector(".button");
+generateButton.addEventListener('click', (event) => {
+	event.preventDefault();
+	const lengthNumber = document.querySelector(".chislo").value;
+	const letters = "qazwsxedcrfvt1gbyhnujmioklQPWOEIRUTY48AL7SK5DJFH36G2ZXCV9BNM";
+	const lettersMassive = letters.split('');	
+	let finalPassword = "";
+
+	for (i=0; i<lengthNumber; i++) {
+		const lettersLength = lettersMassive.length;
+		let randomNumber = Math.floor(Math.random() * Math.floor(lettersLength));
+		finalPassword = finalPassword + lettersMassive[randomNumber];
+	}
+
+	const passwordField = document.querySelector(".password");
+	passwordField.innerText = finalPassword;
+})
+
+const numberInput = document.querySelector(".chislo");
+numberInput.addEventListener('change', () => {
+	if(numberInput.value>30) {
+		let minusValue = numberInput.value - 30;
+		numberInput.value = numberInput.value - minusValue;
+	}
+})
+
+numberInput.addEventListener('input', () => {
+	if(numberInput.value>30) {
+		let minusValue = numberInput.value - 30;
+		numberInput.value = numberInput.value - minusValue;
+	}
+})
+
