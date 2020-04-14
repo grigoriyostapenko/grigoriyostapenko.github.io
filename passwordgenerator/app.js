03b517eccf19efa87f2efa567f2f670343906181
Kelvin <grishakelvin@gmail.com>
2020-04-14 21:00:10 +0300

    fix chance of numbers x2

--- passwordgenerator/app.js
+++ passwordgenerator/app.js
@@ -2,7 +2,7 @@ const generateButton = document.querySelector(".button");
 generateButton.addEventListener('click', (event) => {
 	event.preventDefault();
 	const lengthNumber = document.querySelector(".chislo").value;
-	const letters = "qazw9sx7e8dcrf8vt1gbyhnu4jm8i9okl541QPWOEIR2UTY48AL7SK5D6JFH43664G2ZXCV9BNM";
+	const letters = "qazw9sx7e8dcrf8vt1gby8hnu4jm8i9okl541QPWOEIR2UTY48AL37SK55D6JFH4143664G2ZXCV9BN0M";
 	const lettersMassive = letters.split('');	
 	let finalPassword = "";
 
