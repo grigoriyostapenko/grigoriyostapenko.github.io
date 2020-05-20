fa8d135a03ad348b7e8c8f2a9189f21971cfe635
Kelvin <grishakelvin@gmail.com>
2020-05-21 01:03:09 +0300

    finally done

--- informatics-project/src/js/app.js
+++ informatics-project/src/js/app.js
@@ -2,3 +2,10 @@ const auditory = document.querySelectorAll(".ls-desc__audit");
 for (let k=0; k<auditory.length; k++) {
 	auditory[k].innerText = k+1+ ". " + auditory[k].innerText + ":";
 }
+
+$("#body").append('<footer class="ls-footer"><div class="container"><h3 class="ls-footer__text">Автор проекту: Остапенко Григорій</h3></div></footer>');
+
+const kkkkkkk = document.querySelectorAll(".ls-buy__item");
+for (let k=0; k<kkkkkkk.length; k++) {
+	kkkkkkk[k].innerText = k+1+ ". " + kkkkkkk[k].innerText;
+}
